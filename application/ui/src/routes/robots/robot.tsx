fcb46bd124529b75bfa651c95b04c3ddaca945ac
Mark Redeman <mark.s.redeman@intel.com>
2026-01-20 09:58:07 +0100

    feat(ui): Identify robot button

--- application/ui/src/routes/robots/robot.tsx
+++ application/ui/src/routes/robots/robot.tsx
@@ -2,8 +2,18 @@ import { Button, ButtonGroup, Flex, Item, TabList, TabPanels, Tabs } from '@geti
 import { Outlet, useLocation, useParams } from 'react-router-dom';
 
 import { $api } from '../../api/client';
+import { IdentifyRobotButton } from '../../features/robots/identify-robot-button';
+import { useRobot } from '../../features/robots/use-robot';
 import { paths } from '../../router';
 import { getPathSegment } from '../../utils';
+
+const IdentifyRobot = () => {
+    const robot = useRobot();
+    const { data: robots } = $api.useSuspenseQuery('get', '/api/hardware/robots');
+    const hardwareRobot = robots.find(({ serial_id }) => serial_id === robot.serial_id);
+
+    return <IdentifyRobotButton port_id={hardwareRobot?.port ?? ''} />;
+};
 
 export const Robot = () => {
     const { pathname } = useLocation();
@@ -12,19 +22,9 @@ export const Robot = () => {
         robot_id: string;
     };
 
-    const { data: robot } = $api.useSuspenseQuery('get', '/api/projects/{project_id}/robots/{robot_id}', {
-        params: { path: { project_id: params.project_id, robot_id: params.robot_id } },
-    });
-    const identifyMutation = $api.useMutation('post', '/api/hardware/identify');
-
-    const onIdentify = identifyMutation.isPending
-        ? undefined
-        : () => {
-              identifyMutation.mutate({ body: robot });
-          };
-
+    const selectedKey = getPathSegment(pathname, 5);
     return (
-        <Tabs aria-label='Robot configuration navigation' selectedKey={getPathSegment(pathname, 5)} height='100%'>
+        <Tabs aria-label='Robot configuration navigation' selectedKey={selectedKey} height='100%'>
             <Flex>
                 <TabList
                     width='100%'
@@ -56,10 +56,8 @@ export const Robot = () => {
                             'var(--spectrum-alias-border-size-thick) solid var(--spectrum-global-color-gray-300)',
                     }}
                 >
-                    <ButtonGroup>
-                        <Button variant='secondary' onPress={onIdentify}>
-                            Identify
-                        </Button>
+                    <ButtonGroup isHidden={selectedKey !== paths.project.robots.controller(params)}>
+                        <IdentifyRobot />
                         <Button variant='secondary'>Connect</Button>
                     </ButtonGroup>
                 </div>
