afe5b2ac7110d8fb9a57550e438e249472f29e26
Mark Redeman <markredeman@gmail.com>
2026-02-27 18:00:41 +0100

    Initial attempt to import from hugging face

--- application/backend/src/services/__init__.py
+++ application/backend/src/services/__init__.py
@@ -2,6 +2,7 @@ from robots.robot_service import RobotService
 
 from .dataset_service import DatasetService
 from .job_service import JobService
+from .model_import_service import ModelImportService
 from .model_service import ModelService
 from .project_camera_service import ProjectCameraService
 from .project_service import ProjectService
@@ -10,6 +11,7 @@ from .snapshot_service import SnapshotService
 __all__ = [
     "DatasetService",
     "JobService",
+    "ModelImportService",
     "ModelService",
     "ProjectCameraService",
     "ProjectService",
