32f18ca7535a7518cf0b9e200c296bc51f2b8c78
Mark Redeman <markredeman@gmail.com>
2026-03-16 09:38:38 +0100

    feat: Allow removing failed jobs

--- application/backend/src/api/job.py
+++ application/backend/src/api/job.py
@@ -23,6 +23,15 @@ async def list_jobs(
     return await job_service.get_job_list()
 
 
+@router.delete("/{job_id}")
+async def delete_job(
+    job_id: Annotated[UUID, Depends(get_job_id)],
+    job_service: Annotated[JobService, Depends(get_job_service)],
+) -> None:
+    """Delete a job. Only allows deleting failed jobs"""
+    await job_service.delete_job(job_id)
+
+
 @router.post(":train")
 async def submit_train_job(
     job_service: Annotated[JobService, Depends(get_job_service)],
