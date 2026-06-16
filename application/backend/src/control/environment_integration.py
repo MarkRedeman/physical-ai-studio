62bfd53efcd3fbfa0da38d1d6052c53e9b5c4e5d
Mark Redeman <markredeman@gmail.com>
2026-06-16 09:42:51 +0200

    fix: fix inference

--- application/backend/src/control/environment_integration.py
+++ application/backend/src/control/environment_integration.py
@@ -148,8 +148,7 @@ class EnvironmentIntegration:
         return Observation(
             state=state,
             images=images,
-
-            #task=["Move the dice into the cup"], # TODO: Implement tasks.
+            task=["Move the dice into the cup"], # TODO: Implement tasks.
             # Move the dice into the cup
 
         )
